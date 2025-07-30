690d9e3136a816b513c47404b8cf4e088937e90a
quanghehe <havietquangss@gmail.com>
2025-07-30 14:58:49 +0700

    chagne code in products sql

--- dbt/superstore_dbt/models/mart/dim_products.sql
+++ dbt/superstore_dbt/models/mart/dim_products.sql
@@ -1,17 +1,13 @@
--- depends_on: {{ ref('stg_order') }}
+-- depends_on: {{ ref('stg_products') }}
 {{ config(
     materialized='table',
-    unique_key='id'
+    unique_key='product_key'
 ) }}
 
-with source_product as (
-    select 
-        product_id,
-        product_name,
-        category,
-        sub_category
-    from {{ ref('stg_order') }}
-)
-
-select * from source_product
-
+select
+  row_number() over(order by product_id) as product_key,
+  product_id,
+  product_name,
+  category,
+  sub_category
+from {{ ref('stg_products') }}
