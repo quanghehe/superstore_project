3704ae1a384bff3306e73fdb448201ac31450fe7
quanghehe <havietquangss@gmail.com>
2025-07-30 14:48:45 +0700

    change code in customer

--- dbt/superstore_dbt/models/staging/stg_customers.sql
+++ dbt/superstore_dbt/models/staging/stg_customers.sql
@@ -1,12 +1,12 @@
 -- depends_on: {{ ref('stg_order') }}
 {{ config(
     materialized='table',
-    unique_key='customer_key'
+    unique_key='customer_id'
 ) }}
 
-select
-    row_number() over(order by customer_id) as customer_key,
+select distinct
     customer_id,
     customer_name,
     segment
-from {{ref('stg_customers')}}
+from {{ ref('stg_order') }}
+where customer_id is not null
